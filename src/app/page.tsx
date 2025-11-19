84d9bda221a0e56ff0e517bc600e56930a0c6948
Kirtan Chitalia <kirtanchitalia07@gmail.com>
2025-11-19 21:43:29 +0530

    initial changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Header from "@/app/header/page";
 import Image from "next/image";
 import Footer from "@/app/footer/page"
-import HomePage from "@/app/reader/home/page";
+import HomePage from "@/app/home/page";
 import ExplorePage from "./reader/explore/page";
 import LeaderboardPage from "./leaderboard/page";
 export default function Home() {
